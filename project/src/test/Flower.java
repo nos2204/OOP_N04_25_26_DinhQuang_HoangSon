716179ddb7d16b9876d37e129acf25228b4be040
Quang1368 <23010498@st.phenikaa-uni.edu.vn>
2025-09-05 08:31:24 +0000

    yeah

---
+++ project/src/test/Flower.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Flower {
+    int petalCount = 0;
+    String s = new String("null");
+    Flower(int petals) { petalCount = petals; }
+    Flower(String ss) { s = ss; }
+    Flower(String s, int petals) {
+    this(petals);
+
+    this.s = s;
+}
+Flower() { this("hi", 47); } 
+}
