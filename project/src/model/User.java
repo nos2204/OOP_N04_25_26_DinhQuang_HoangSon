6f34bf8801a72132c67f285f1e3672df9fc5a175
Quang1368 <23010498@st.phenikaa-uni.edu.vn>
2025-09-08 11:04:20 +0000

    hi

---
+++ project/src/model/User.java
@@ -0,0 +1,19 @@
+package model;
+
+public class User{
+    private static String userName;
+    private static String userPassword;
+
+    public static String getUser(){
+        return userName;
+    }
+
+    public static String getPassword(){
+        return userPassword;
+    }
+
+    public static void setUser(String name, String password){
+        userName = name;
+        userPassword = password;
+    }
+}
