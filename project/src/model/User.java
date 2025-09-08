888fc888c0d527e1a4cec6f96305a9176342506f
Le Thu Nguyen <nglthu1967@gmail.com>
2025-09-08 11:11:16 +0000

    update

--- project/src/model/User.java
+++ project/src/model/User.java
@@ -1,24 +1,4 @@
-package model;
-<<<<<<< HEAD
-public class User {
 
-        private static String userName;
-        private static String userAddress;
-
-        public static String getUser() {
-                return userName;
-        }
-
-        public static String getAddress() {
-                return userAddress;
-        }
-
-        public static void setUser(String name, String address) {
-                userName = name;
-                userAddress = address;
-        }
-}
-=======
 
 public class User{
     private static String userName;
@@ -37,4 +17,4 @@ public class User{
         userPassword = password;
     }
 }
->>>>>>> 6f34bf8801a72132c67f285f1e3672df9fc5a175
+
